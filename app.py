a3f87a977b8db42ca20762c6574b0cafcd53c4bd
Wang Xu <wangxu627@gmail.com>
2022-08-19 06:47:18 +0800

    hello

---
+++ app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Grey Li (李辉)
+    :url: http://greyli.com
+    :copyright: © 2018 Grey Li
+    :license: MIT, see LICENSE for more details.
+"""
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# the minimal Flask application
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+# bind multiple URL for one view function
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
